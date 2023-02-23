9bcdb8aa7646c86a5b41efa7d65ac60d9b178a6d
Максим <116575421+MaxGid@users.noreply.github.com>
2023-02-24 01:19:57 +0200

    add method

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,26 @@
 import java.io.IOException;
-import java.net.SocketOption;
-import java.net.SocketTimeoutException;
-import java.sql.SQLOutput;
 import java.util.HashMap;
 import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter action: ");
+        String input = reader.nextLine();
+        System.out.println(calc(input));
+    }
+
+    public static String calc(String input) throws Exception {
         int num1;
         int num2;
         int answer;
         int answer2;
         int tens;
         char op;
+        boolean roman;
+        String answerRom;
         String operand1;
         String operand2;
-        String answerRom;
-        String wrong;
-        boolean roman;
         HashMap<String, Integer> map = new HashMap<>();
         map.put("I", 1);
         map.put("II", 2);
@@ -29,78 +33,91 @@ public class Main {
         map.put("IIX", 8);
         map.put("IX", 9);
         map.put("X", 10);
-        Scanner reader = new Scanner(System.in);
-        System.out.print("Enter action: ");
-        operand1 = reader.next();
-        try {
-            num1 = Integer.parseInt(operand1);
-            if (num1<0||num1>10) {throw new IOException();}
-            op = reader.next().charAt(0);
-            operand2 = reader.next();
-            num2 = Integer.parseInt(operand2);
-            if (num2<0||num2>10) {throw new IOException();}
-            roman = false;
-         } catch (NumberFormatException e) {
-            num1 = map.get(operand1);
-            op = reader.next().charAt(0);
-            operand2 = reader.next();
-            num2 = map.get(operand2);
-            roman = true;
-        }
-        wrong = reader.nextLine();
-        if (wrong=="") {
-        reader.close();
-        switch(op) {
-            case '+': answer = num1 + num2;
-                break;
-            case '-': answer = num1 - num2;
-                break;
-            case '*': answer = num1 * num2;
-                break;
-            case '/': answer = num1 / num2;
-                break;
-            default:  throw new IOException();
-        }
-        if (roman==true) {
-            HashMap<Integer, String> map2 = new HashMap<>();
-            map2.put(1, "I");
-            map2.put(2, "II");
-            map2.put(3, "III");
-            map2.put(4, "IV");
-            map2.put(5, "V");
-            map2.put(6, "VI");
-            map2.put(7, "VII");
-            map2.put(8, "VIII");
-            map2.put(9, "IX");
-            map2.put(10, "X");
-            map2.put(20, "XX");
-            map2.put(30, "XXX");
-            map2.put(40, "XL");
-            map2.put(50, "L");
-            map2.put(60, "LX");
-            map2.put(70, "LXX");
-            map2.put(80, "LXXX");
-            map2.put(90, "XC");
-            map2.put(100, "C");
-            if (answer<1) {throw new IOException();}
-            if (answer<11) {
-                answerRom = map2.get(answer);
-            System.out.println(answerRom);}
-            if (answer<100) {
-                if (answer > 10) {
-                    tens = answer / 10;
-                    answer2 = answer % 10;
-                    if (answer2 == 0) {
-                        answerRom = map2.get(answer);
-                        System.out.print(answerRom);
-                    } else {
-                        operand1 = map2.get(tens * 10);
-                        operand2 = map2.get(answer2);
-                        System.out.println(operand1 + operand2);
+        String[] operands = input.split(" ");
+        if (operands.length==3)
+        {
+            operand1 = operands[0];
+            try {
+                num1 = Integer.parseInt(operand1);
+                if (num1 < 0 || num1 > 10) {
+                    throw new Exception();
+                }
+                op = operands[1].charAt(0);
+                operand2 = operands[2];
+                num2 = Integer.parseInt(operand2);
+                if (num2 < 0 || num2 > 10) {
+                    throw new Exception();
+                }
+                roman = false;
+            } catch (NumberFormatException e) {
+                if (map.get(operand1)==null) {throw new Exception();} else {
+                num1 = map.get(operand1);
+                op = operands[1].charAt(0);
+                operand2 = operands[2];
+                if (map.get(operand2)==null) {throw new Exception();} else {
+                num2 = map.get(operand2);
+                roman = true;}}
+            }
+            switch (op) {
+                case '+':
+                    answer = num1 + num2;
+                    break;
+                case '-':
+                    answer = num1 - num2;
+                    break;
+                case '*':
+                    answer = num1 * num2;
+                    break;
+                case '/':
+                    answer = num1 / num2;
+                    break;
+                default:
+                    throw new Exception();
+            }
+            if (roman) {
+                HashMap<Integer, String> map2 = new HashMap<>();
+                map2.put(1, "I");
+                map2.put(2, "II");
+                map2.put(3, "III");
+                map2.put(4, "IV");
+                map2.put(5, "V");
+                map2.put(6, "VI");
+                map2.put(7, "VII");
+                map2.put(8, "VIII");
+                map2.put(9, "IX");
+                map2.put(10, "X");
+                map2.put(20, "XX");
+                map2.put(30, "XXX");
+                map2.put(40, "XL");
+                map2.put(50, "L");
+                map2.put(60, "LX");
+                map2.put(70, "LXX");
+                map2.put(80, "XXC");
+                map2.put(90, "XC");
+                map2.put(100, "C");
+                if (answer < 1) {throw new IOException();}
+                if (answer < 11) {
+                    answerRom = map2.get(answer);
+                    return (answerRom);
+                }
+                if (answer < 100) {
+                    if (answer > 10) {
+                        tens = answer / 10;
+                        answer2 = answer % 10;
+                        if (answer2 == 0) {
+                            answerRom = map2.get(answer);
+                            return (answerRom);
+                        } else {
+                            operand1 = map2.get(tens * 10);
+                            operand2 = map2.get(answer2);
+                            return (operand1 + operand2);
+                        }
                     }
                 }
-            }} else {System.out.print(answer);}
-    }else {
-           throw new IOException();
-        }}
+            }
+                return (Integer.toString(answer));
+        } else {
+            throw new Exception();
+        }
+    }
 }
